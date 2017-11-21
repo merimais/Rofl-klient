791ff870c1a3c754f71e24f921b07aeface5ff99
Merima <meis16ab@student.cbs.dk>
2017-11-21 11:07:28 +0100

    users javascript class created

---
+++ js/users.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+
+    SDK.User.loadNav();
+const $listAllUsers = $("#listAllUsers");
+
+SDK.Book.findAllUsers((err, users) => {
+    users.forEach((user) => {
+
+    $listAllUsers.append(`  
+            <div class="row">
+            <div class="col-lg-6">
+            <div class="page-header">
+                <table>
+                    <tr>
+                        <td>${user.email}</td>
+                        <td>${user.firstName}</td>
+                        <td>${user.lastName}</td>
+                        <td>${user.gender}</td>
+                        <td>${user.major}</td>
+                        <td>${user.semester}</td>
+                        <td>${user.description}</td>
+                    </tr>
+                </table>
+            </div>
+            `
+);
+});
+
+});
+});
