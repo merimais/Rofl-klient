732c057a3b5f520cafa8155aa74d0393e272f7bc
Merima <meis16ab@student.cbs.dk>
2017-11-21 11:04:53 +0100

    createUser javascript class created

---
+++ js/createUser.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+
+    SDK.User.loadNav();
+
+$("#create-button").click(() => {
+    const email = $("#inputEmail").val();
+const firstName = $("#inputFirstname").val();
+const lastName = $("#inputLastname").val();
+const description = $("#inputDescription").val();
+const gender = $("#inputGender").val();
+const major = $("#inputMajor").val();
+const semester = $("#inputSemester").val();
+const password = $("#inputPassword").val();
+
+SDK.Book.createUser(password, firstName, lastName, email, description, gender, major, semester, (err, data)=> {
+    if (err && err.xhr.status === 401) {
+    $(".form-group").addClass("has-error");
+} else {
+    window.location.href = "homepage.html";
+}
+});
+
+});
+
+});
