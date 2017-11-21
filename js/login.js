d22baae75e1e3cc5678a2044378a0611f3462d85
Merima <meis16ab@student.cbs.dk>
2017-11-21 11:06:17 +0100

    login javascript class created

---
+++ js/login.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+
+    SDK.User.loadNav();
+
+$("#login-button").click(() => {
+    const email = $("#inputEmail").val();
+const password = $("#inputPassword").val();
+
+SDK.User.login(email, password, (err, data) => {
+    if (err && err.xhr.status === 401) {
+    $(".form-group").addClass("has-error");
+}
+else if (err){
+    console.log("Bad stuff happened")
+} else {
+    window.location.href = "homepage.html";
+}
+});
+
+});
+
+});
