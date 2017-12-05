9e08c1105ef3f51630d937c758eb417ade29e941
Merima <meis16ab@student.cbs.dk>
2017-12-05 11:57:20 +0100

    Created "Go Back" button in createEvent html and javascript file

--- js/createEvent.js
+++ js/createEvent.js
@@ -9,13 +9,17 @@ $(document).ready(() => {
         const endDate = $("#inputEndDate").val();
         const description = $("#inputDescription").val();
 
-        SDK.Book.createEvent(owner_id, title, startDate, endDate, description, (err, data)=> {
+        SDK.Event.createEvent(owner_id, title, startDate, endDate, description, (err, data)=> {
             if (err && err.xhr.status === 401) {
                 $(".form-group").addClass("has-error");
             } else {
                 window.location.href = "events.html";
             }
         });
+
+    });
+    $("#goback-button").click(() => {
+        window.location.href = "events.html";
 
     });
 
